44c122cb38c46ba8bff5019a80487e72e983b8a3
Lauren <laurencaroleen@gmail.com>
2021-10-27 02:58:31 +0000

    Completed function

--- wordcount.py
+++ wordcount.py
@@ -1,4 +1,19 @@
 """Count words in file."""
 
+def count_words(txt_file):
 
+    poem = open(txt_file)
 
+    dictionary_of_poem = {}
+
+    for line in poem:
+        words = line.split()
+        #print(words)
+        
+        for word in words:
+            dictionary_of_poem[word] = dictionary_of_poem.get(word, 0) + 1
+            
+    for key, value in dictionary_of_poem.items():
+        print(key, value)
+
+count_words("test.txt")
